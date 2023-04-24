6f0c0081db576e577e529e356c8536b4cde0e125
sushilkadam19 <109297378+sushilkadam19@users.noreply.github.com>
2023-04-24 20:38:04 +0530

    updateaks

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,17 +1,18 @@
 data "azurerm_resource_group" "RG" {
-    resource_group_name= "RG-CIGH-0"
+  name: "RG-CIGH-0"
+  location: "Central India"
 }
 
 resource "azurerm_container_registry" "acr01" {
     name = "acr01"
-    resource_group_name = data.azurerm_resource_group.RG.resource_group_name
-    location = azurerm_resource_group.RG01.location
+    resource_group_name = data.azurerm_resource_group.RG.name
+    location = data.azurerm_resource_group.RG.location
     sku = "Premium"
 
 }
 resource "azurerm_kubernetes_cluster" "aks01" {
     name = "AKS01"
-    location = azurerm_resource_group.RG01.location
+    location = data.azurerm_resource_group.RG.location
     resource_group_name = data.azurerm_resource_group.RG.resource_group_name
     dns_prefix = "aks01"
 
@@ -28,7 +29,7 @@ resource "azurerm_kubernetes_cluster" "aks01" {
   }
   
 }
-resource "azurerm_role_assignment" "example" {
+resource "azurerm_role_assignment" "aksras" {
   principal_id                     = azurerm_kubernetes_cluster.aks01.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr01.id
