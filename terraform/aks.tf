74741d48c2761ffd72808b90b7a2ab3824bec755
sushilkadam19 <109297378+sushilkadam19@users.noreply.github.com>
2023-04-24 20:45:32 +0530

    Update aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "RG" {
   #location ="Central India"
 }
 
-resource "azurerm_container_registry" "acr01" {
+resource "azurerm_container_registry" "acr01practice01" {
     name = "acr01"
     resource_group_name = data.azurerm_resource_group.RG.name
     location = data.azurerm_resource_group.RG.location
@@ -13,7 +13,7 @@ resource "azurerm_container_registry" "acr01" {
 resource "azurerm_kubernetes_cluster" "aks01" {
     name = "AKS01"
     location = data.azurerm_resource_group.RG.location
-    resource_group_name = data.azurerm_resource_group.RG.location
+    resource_group_name = data.azurerm_resource_group.RG.name
     dns_prefix = "aks01"
 
     default_node_pool {
