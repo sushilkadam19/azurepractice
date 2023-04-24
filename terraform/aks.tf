88c958fa7ca3f84e4eaacd375a26c359bee966e4
sushilkadam19 <109297378+sushilkadam19@users.noreply.github.com>
2023-04-24 20:42:06 +0530

    Update aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -13,7 +13,7 @@ resource "azurerm_container_registry" "acr01" {
 resource "azurerm_kubernetes_cluster" "aks01" {
     name = "AKS01"
     location = data.azurerm_resource_group.RG.location
-    resource_group_name = data.azurerm_resource_group.RG.resource_group_name
+    resource_group_name = data.azurerm_resource_group.RG.location
     dns_prefix = "aks01"
 
     default_node_pool {
