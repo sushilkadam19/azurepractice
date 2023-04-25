20eedfab8d7612e9f1d6753461f4514a3bc731d3
sushilkadam19 <109297378+sushilkadam19@users.noreply.github.com>
2023-04-25 17:12:56 +0530

    var

--- terraform/foreach.tf
+++ terraform/foreach.tf
@@ -1,3 +1,14 @@
+variable "resourcegroup" {
+    type = string
+    default = "rg01"
+}
+
+
+variable "rglocation" {
+    type = string
+    location = "Central India"
+}
+
 resource "azurerm_resource_group" "RG01" {
     name = var.resourcegroup
     location = var.rglocation
