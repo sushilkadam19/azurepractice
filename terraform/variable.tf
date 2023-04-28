e7a82737a8f886e454f38087cf4a9979bb28db4d
sushilkadam19 <109297378+sushilkadam19@users.noreply.github.com>
2023-04-28 19:11:27 +0530

    updated aks

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -11,7 +11,7 @@ variable "AKS_ACR" {
   }
   ))
   default = {
-    "one" = {
+    1 = {
     aks_name        =  "AKS01"
     aks_node_count = 1
     aks_dns_prefix = "AKS01"
@@ -24,7 +24,7 @@ variable "AKS_ACR" {
     acr_name       = "acr01practice01"
     acr_sku        = "Premium"
     }
-   "two" = {
+   2 = {
     aks_name        =  "AKS02"
     aks_node_count = 1
     aks_dns_prefix = "AKS02"
