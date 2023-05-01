c26330ad512d91a446151070c1b34a9af9f44ea4
sushilkadam19 <109297378+sushilkadam19@users.noreply.github.com>
2023-05-02 00:35:18 +0530

    upd

--- terraform/count.tf
+++ terraform/count.tf
@@ -34,7 +34,17 @@ resource "azurerm_network_security_group" "nsg01" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-    
+    security_rule  {
+    name                       = "test124"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
 }
 
