0ce0745dcb47564ad226368eef5373e5bd06857e
sushilkadam19 <109297378+sushilkadam19@users.noreply.github.com>
2023-05-02 00:00:11 +0530

    update

--- terraform/count.tf
+++ terraform/count.tf
@@ -9,6 +9,18 @@ resource "azurerm_network_security_group" "nsg01" {
     name = "nsg01"
     location = azurerm_resource_group.testrg-count[1].location
     resource_group_name = azurerm_resource_group.testrg-count[1].name
+
+    security_rule {
+    name                       = "test123"
+    priority                   = 160
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_virtual_network" "vnet01" {
